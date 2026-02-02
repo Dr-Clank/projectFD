d95d5552b6fecba06fdc58801c4a7c8eb660b467
Dr-Clank <lolo.max@hotmail.com>
2026-02-02 18:33:46 +0100

    Ajouts de la class Search et du composant fixtures pour test

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Search;
+use DateTime;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $product1 = new Search();
+        $product1->setLatitude('47,3167');
+        $product1->setLongitude('5,0167');
+        $product1->setPrevision(3);
+        $product1->setSearchDate(new DateTime());
+        $product1->setCity('Dijon');
+        
+        $manager->persist($product1);
+
+        $product2 = new Search();
+        $product2->setLatitude('40,7143');
+        $product2->setLongitude('-74,006');
+        $product2->setPrevision(3);
+        $product2->setSearchDate((new DateTime())->modify('-1 day'));
+        $product2->setCity('New york');
+
+        $manager->persist($product2);
+
+        $manager->flush();
+    }
+}
---
+++ src/Entity/Search.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SearchRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SearchRepository::class)]
+class Search
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 9, scale: 6)]
+    private ?string $latitude = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 9, scale: 6)]
+    private ?string $longitude = null;
+
+    #[ORM\Column]
+    private ?int $prevision = null;
+
+    #[ORM\Column]
+    private ?\DateTime $searchDate = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $city = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLatitude(): ?string
+    {
+        return $this->latitude;
+    }
+
+    public function setLatitude(string $latitude): static
+    {
+        $this->latitude = $latitude;
+
+        return $this;
+    }
+
+    public function getLongitude(): ?string
+    {
+        return $this->longitude;
+    }
+
+    public function setLongitude(string $longitude): static
+    {
+        $this->longitude = $longitude;
+
+        return $this;
+    }
+
+    public function getPrevision(): ?int
+    {
+        return $this->prevision;
+    }
+
+    public function setPrevision(int $prevision): static
+    {
+        $this->prevision = $prevision;
+
+        return $this;
+    }
+
+    public function getSearchDate(): ?\DateTime
+    {
+        return $this->searchDate;
+    }
+
+    public function setSearchDate(\DateTime $searchDate): static
+    {
+        $this->searchDate = $searchDate;
+
+        return $this;
+    }
+
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+
+    public function setCity(?string $city): static
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+}
